473b4d42af04746614e763252ef9032f3391e7da
Daniel Legan <legan.daniel@gmail.com>
2022-09-24 12:29:30 -0500

    created form

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log('in script.js');
+
+let employees = [];
+
+$(document).ready(readyNow);
+
+function readyNow(){
+    console.log('the DOM is loaded!');
+
+    $('#employeeForm').on('submit', addEmployee);
+}
+
+function addEmployee(event){
+
+    event.preventDefault();
+
+    console.log('in addEmployee');
+
+    let employee = {
+        firstName: $('#firstNameInput').val(),
+        lastName: $('#lastNameInput').val(),
+        ID: $('#idInput').val(),
+        title: $('#titleInput').val(),
+        annualSalary: $('#annualSalaryInput').val()
+    }
+    console.log('employee is', employee);
+
+    employees.push(employee);
+
+    console.log('employees is', employees);
+
+    render();
+}
+
+function render(){
+    
+}
